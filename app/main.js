9e7dabaeee0b3940cb4a132ea95af7561b961dc5
bhanu1012 <bhanubasyan@gmail.com>
2026-06-16 11:55:38 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -5,9 +5,13 @@ console.log("Logs from your program will appear here!");
 
 
  const server = net.createServer((socket) => {
-  socket.write('HTTP/1.1 200 OK\r\n\r\n');
-   socket.on("close", () => {
-     socket.end();
-   });
+  socket.on("data" , data =>{
+
+ 
+  const path = data.toString().split(" ")[1];
+  const responseStatus = path === "/" ? "200 OK" : "404 Not Found";
+  socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
+   socket.end(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
  });
+  });
  server.listen(4221, "localhost");
